eb0c9e1535405df8046fe6500548099d59f0192b
YunaJangqe <lis3277@gamil.com>
2026-01-13 21:14:54 +0900

    commit

--- section02/chapter01.js
+++ section02/chapter01.js
@@ -8,5 +8,29 @@ let f6 = "";
 let f7 = 0n;
 
 if(!f1) {
-    console.log("falsy");
+    //console.log("falsy");
 }
+
+// 2. Truthy 한 값
+// -> 7가지 Falsy 한 값을 제외한 나머지 모든 값
+let t1 = "hello";
+let t2 = 123;
+let t3 = [];
+let t4 = {};
+let t5 = () => {};
+
+if(t1) {
+    //console.log("truthy");
+}
+
+// 3. 활용 사례
+function printName(person) {
+    if(!person) {
+        console.log("person의 값이 없음");
+        return;
+    }
+    console.log(person.name);
+}
+
+let person = null;
+printName(person);
