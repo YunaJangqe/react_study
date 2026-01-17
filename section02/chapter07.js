f499be7ed8507feb51a6c5344167475badcdf1fb
YunaJangqe <lis3277@gamil.com>
2026-01-17 18:03:29 +0900

    commti

--- section02/chapter07.js
+++ section02/chapter07.js
@@ -19,4 +19,16 @@ const shiftedItem = arr3.shift();
 // 배열의 맨 앞에 새로운 요소를 추가하는 메서드
 let arr4 = [1, 2, 3];
 const newLength2 = arr4.unshift(0);
-console.log(arr4);
+
+// 5. slice
+// 마치 가위처럼, 배열의 특정 범위를 잘라내서 새로운 배열로 변환
+let arr5 = [1, 2, 3, 4, 5];
+let sliced = arr5.slice(2, 5);
+let sliced2 = arr5.slice(-3);
+ 
+// 6. concat
+ // 두개의 서로 다른 배열을 이어 붙여서 새로운 배열로 반환
+let arr6 = [1, 2];
+let arr7 = [3, 4];
+let concatedArr = arr6.concat(arr7);
+console.log(concatedArr);
