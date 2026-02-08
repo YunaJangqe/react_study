c24d0963839cb7f3a6754ca89dfd26a849464895
YunaJangqe <lis3277@gamil.com>
2026-02-08 16:55:07 +0900

    commit

--- section09/src/App.jsx
+++ section09/src/App.jsx
@@ -2,7 +2,8 @@ import './App.css';
 import Header from './components/Header';
 import Editor from './components/Editor';
 import List from './components/List';
-import { useState, useRef } from 'react';
+import { useState, useRef, useReducer } from 'react';
+import Exam from './components/Exam';
 
 const mokDate = [
   {
@@ -25,30 +26,50 @@ const mokDate = [
   },
 ];
 
+function reducer(state, action) {
+  switch(action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) => item.id === action.targetId ? { ...item, isDone: !item.isDone } : item );
+    case "DELETE":
+      return state.filter((item) => item.id !== action.targetId);      
+  }
+}
+
 function App() {
-  const [ todos, setTodos ] = useState(mokDate);
+  const [ todos, dispatch ] = useReducer(reducer, mokDate);
   const idRef = useRef(3);
 
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current ++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    };
-    setTodos([newTodo, ...todos]);
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      }
+    })
   };
 
   const onUpdate = (targetId) => {
-    setTodos(todos.map((todo) => todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo ))
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId
+    })
   };
 
   const onDelete = (targetId) => {
-    setTodos(todos.filter((todo) => todo.id !== targetId))
+    dispatch({
+      type: "DELETE",
+      targetId: targetId
+    })
   };
 
   return (
     <div className='App'>
+      {/* <Exam /> */}
       <Header />
       <Editor onCreate={ onCreate } />
       <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />    
---
+++ section09/src/components/Exam.jsx
@@ -0,0 +1,40 @@
+import { useReducer } from 'react';
+
+function reducer(state, action) {
+    switch(action.type) {
+        case "INCREASE":
+            return state + action.data;
+        case "DECREASE":
+            return state - action.data;
+        default:
+            return state;
+    }
+}
+
+const Exam = () => {
+    // dispatch : 발송하다
+    // -> 상태 변화가 있어야 한다는 사실을 알리는, 발송하는 함수
+    const [state, dispatch] = useReducer(reducer, 0);
+
+    const onClickPlus = () => {
+        dispatch({
+            type: "INCREASE",
+            data: 1
+        });
+    }
+
+    const onClickMinus = () => {
+        dispatch({
+            type: "DECREASE",
+            data: 1
+        });
+    }
+
+    return <div>
+        <h1>{state}</h1>
+        <button onClick={onClickPlus}>+</button>
+        <button onClick={onClickMinus}>-</button>
+    </div>
+};
+
+export default Exam;
