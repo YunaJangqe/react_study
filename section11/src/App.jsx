5f1bc3032ab77054a866b6bb04230001415287ae
YunaJangqe <lis3277@gamil.com>
2026-02-20 06:46:40 +0900

    commit

--- section11/src/App.jsx
+++ section11/src/App.jsx
@@ -4,12 +4,7 @@ import Diary from './pages/Diary';
 import New from './pages/New';
 import Notfound from './pages/Notfound';
 import { Routes, Route, Link, useNavigate } from 'react-router-dom';
-
-import emotion1 from './assets/emotion1.png';
-import emotion2 from './assets/emotion2.png';
-import emotion3 from './assets/emotion3.png';
-import emotion4 from './assets/emotion4.png';
-import emotion5 from './assets/emotion5.png';
+import { getEmotionImage } from './util/get-emotion-image';
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -22,19 +17,19 @@ function App() {
 
   return (
     <>
-      <div>
+      {/* <div>
         <img src={"/emotion1.png"} />
         <img src={"/emotion2.png"} />
         <img src={"/emotion3.png"} />
         <img src={"/emotion4.png"} />
         <img src={"/emotion5.png"} />
-      </div>
+      </div> */}
       <div>
-        <img src={emotion1} />
-        <img src={emotion2} />
-        <img src={emotion3} />
-        <img src={emotion4} />
-        <img src={emotion5} />
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
       </div>
       <div>
         <Link to={"/"}>Home</Link>
