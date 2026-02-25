cc37c0edf5c6b638150365844e2cb9a098c43691
YunaJangqe <lis3277@gamil.com>
2026-02-25 21:09:14 +0900

    commit

--- section11/src/App.jsx
+++ section11/src/App.jsx
@@ -13,15 +13,21 @@ import { useReducer, useRef, createContext } from 'react';
 const mockData = [
   {
     id: 1,
-    createdDate: new Date().getTime(),
+    createdDate: new Date('2026-02-21').getTime(),
     emotionId: 1,
     content: "1번 일기 내용",
   },
   {
     id: 2,
-    createdDate: new Date().getTime(),
+    createdDate: new Date('2026-02-20').getTime(),
     emotionId: 2,
     content: "2번 일기 내용",
+  },
+  {
+    id: 3,
+    createdDate: new Date('2026-01-10').getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
   },
 ]
 
@@ -38,8 +44,8 @@ const reducer = (state, action) => {
   }
 };
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
